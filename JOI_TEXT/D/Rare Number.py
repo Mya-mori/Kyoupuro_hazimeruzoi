a74513f7fae37f6335c56e3ed0f2ad1e2e437155
daisuke_harato <daikon0313@yahoo.co.jp>
2023-03-01 15:27:20 +0000

    2023-03-02

--- JOI_TEXT/D/Rare Number.py
+++ JOI_TEXT/D/Rare Number.py
@@ -5,8 +5,21 @@ A = list(map(int,input().split()))
 A.sort()
 
 d = dict(Counter(A))
-
 for i in d:
     if min(list(d.values())) == d[i]:
         print(i)
         exit()
+
+'''
+l = ['a', 'a', 'a', 'a', 'b', 'c', 'c']
+
+print(l.count('a'))
+# 4
+
+print(l.count('b'))
+# 1
+
+d = dict(k1=1, k2=2, k3=3)
+print(d)
+# {'k1': 1, 'k2': 2, 'k3': 3}
+'''
